4bc48103e488ada55ea026b8a6cda93d8ef6114d
div72 <60045611+div72@users.noreply.github.com>
2021-07-19 16:33:39 +0300

    cd: windows initial

--- ci/test/04_install.sh
+++ ci/test/04_install.sh
@@ -35,6 +35,7 @@ if [ -z "$DANGER_RUN_CI_ON_HOST" ]; then
                   --mount type=bind,src=$CCACHE_DIR,dst=$CCACHE_DIR \
                   --mount type=bind,src=$DEPENDS_DIR,dst=$DEPENDS_DIR \
                   --mount type=bind,src=$PREVIOUS_RELEASES_DIR,dst=$PREVIOUS_RELEASES_DIR \
+                  --mount type=tmpfs,dst=/tmp \
                   -w $BASE_ROOT_DIR \
                   --env-file /tmp/env \
                   --name $CONTAINER_NAME \
